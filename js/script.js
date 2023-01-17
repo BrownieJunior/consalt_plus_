0585cf8db823f48cfeda362b8da63ae1b77c621c
“BrownieJunior” <administrator@MacBook-Pro-Administrator.local>
2023-01-17 18:17:28 +0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,47 @@
+//tabs
+
+var tabNavs = document.querySelectorAll(".nav-tab");
+var tabPanes = document.querySelectorAll(".tab-pane");
+
+for (var i = 0; i < tabNavs.length; i++) {
+  tabNavs[i].addEventListener("click", function (e) {
+    e.preventDefault();
+    var activeTabAttr = e.target.getAttribute("data-tab");
+
+    for (var j = 0; j < tabNavs.length; j++) {
+      var contentAttr = tabPanes[j].getAttribute("data-tab-content");
+
+      if (activeTabAttr === contentAttr) {
+        tabNavs[j].classList.add("active");
+        tabPanes[j].classList.add("active");
+      } else {
+        tabNavs[j].classList.remove("active");
+        tabPanes[j].classList.remove("active");
+      }
+    }
+  });
+}
+//swiper
+let swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  speed: 800,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    dragSize: "400",
+  },
+});
